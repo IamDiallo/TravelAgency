b0a9df2444111fdef4892117b6f021600c9d6e69
diawara24 <mourtallafatydiawara@gmail.com>
2021-04-12 18:36:10 +0200

    Update for jax-rs, jaw-ws and the php client

--- travels.management.web/src/travels/management/web/data/DestinationName.java
+++ travels.management.web/src/travels/management/web/data/DestinationName.java
@@ -9,58 +9,94 @@ public class DestinationName {
 	String cityName;
 	int tyDest;
 	int city_id;
+	String img;
    
 	public DestinationName() {
 		super();
 	}
 	
-	public DestinationName(int id, String nameDest, int tyDest, int city_id, String cityName) {
+	
+	
+	public DestinationName(int id, String nameDest, String cityName, int tyDest, int city_id, String img) {
 		super();
 		this.id = id;
 		this.nameDest = nameDest;
+		this.cityName = cityName;
 		this.tyDest = tyDest;
 		this.city_id = city_id;
-		this.cityName = cityName;
+		this.img = img;
 	}
-	
+
+
+
 	public int getId() {
 		return id;
 	}
-	
+
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
+
+
 	public String getNameDest() {
 		return nameDest;
 	}
-	
+
+
+
 	public void setNameDest(String nameDest) {
 		this.nameDest = nameDest;
 	}
-	
-	public int getTyDest() {
-		return tyDest;
-	}
-	
-	public void setTyDest(int tyDest) {
-		this.tyDest = tyDest;
-	}
-	
-	public int getCity_id() {
-		return city_id;
-	}
-	
-	public void setCity_id(int city_id) {
-		this.city_id = city_id;
-	}
-	
+
+
+
 	public String getCityName() {
 		return cityName;
 	}
-	
+
+
+
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
+	}
+
+
+
+	public int getTyDest() {
+		return tyDest;
+	}
+
+
+
+	public void setTyDest(int tyDest) {
+		this.tyDest = tyDest;
+	}
+
+
+
+	public int getCity_id() {
+		return city_id;
+	}
+
+
+
+	public void setCity_id(int city_id) {
+		this.city_id = city_id;
+	}
+
+
+
+	public String getImg() {
+		return img;
+	}
+
+
+
+	public void setImg(String img) {
+		this.img = img;
 	}
 
 
